e9715ff390550f043d41f83f47c659e34461ce35
la55u <andras9405@gmail.com>
2022-08-01 14:01:04 +0200

    add error boundary

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,15 @@
 import type { NextPage } from "next";
 import { Suspense } from "react";
 import { useListPets } from "../api/api";
+import { ErrorBoundary } from "react-error-boundary";
 
 const Home: NextPage = () => {
   return (
-    <Suspense fallback={<p>Loading...</p>}>
-      <Contents />
-    </Suspense>
+    <ErrorBoundary FallbackComponent={ErrorFallback}>
+      <Suspense fallback={<p>Loading...</p>}>
+        <Contents />
+      </Suspense>
+    </ErrorBoundary>
   );
 };
 
@@ -17,5 +20,14 @@ const Contents = () => {
 
   return <pre>{JSON.stringify(data.data, null, 4)}</pre>;
 };
+
+function ErrorFallback({ error, resetErrorBoundary }: any) {
+  return (
+    <div role="alert">
+      <p>Something went wrong:</p>
+      <pre>{error.message}</pre>
+    </div>
+  );
+}
 
 export default Home;
